f657da391147d6e710c87312316bbbc379c23e8b
shaoqun <t-qun.shao>
2019-08-14 11:17:45 +0800

    first commit

---
+++ ghost-eureka/src/main/java/com/ghost/eureka/GhostEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.ghost.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GhostEurekaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GhostEurekaApplication.class, args);
+    }
+}
+
